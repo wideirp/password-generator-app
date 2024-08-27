4efa584ea30481e814d9e5dcc17098edb87df9b5
wideirp <51214984+wideirp@users.noreply.github.com>
2024-08-26 23:22:07 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TextField from "./components/TextField.vue";
+</script>
+
+<template>
+  <h1>Password Generator</h1>
+  <TextField />
+</template>
+
+<style scope lang="scss">
+@use "./assets/sass/variables" as *;
+
+h1 {
+  @include font-heading-m($color-grey);
+  text-align: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "normalize.css";
+import "./assets/sass/global.scss";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
