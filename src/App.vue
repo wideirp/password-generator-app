71d658705c18a1f51bfe6c57af38ad55cea6717a
wideirp <51214984+wideirp@users.noreply.github.com>
2024-08-28 10:23:39 -0500

    Add slider component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import PasswordDisplay from "./components/PasswordDisplay.vue";
+import InteractiveSlider from "./components/InteractiveSlider.vue";
 </script>
 
 <template>
   <main>
     <h1>Password Generator</h1>
     <PasswordDisplay password="678asdf687a@/." />
+    <section>
+      <InteractiveSlider />
+    </section>
   </main>
 </template>
 
@@ -14,11 +18,17 @@ import PasswordDisplay from "./components/PasswordDisplay.vue";
 
 main {
   margin: auto 1rem;
+  color: $color-almost-white;
 }
 
 h1 {
   @include font-heading-m();
   color: $color-grey;
   text-align: center;
+}
+
+section {
+  background: $color-dark-grey;
+  margin-top: 1rem;
 }
 </style>
---
+++ src/components/InteractiveSlider.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from "vue";
+
+const characterLength = ref("10");
+</script>
+
+<template>
+  <fieldset>
+    <label for="slider">Character Length</label>
+    <input
+      type="range"
+      name="slider"
+      id="slider"
+      min="6"
+      max="16"
+      step="1"
+      v-model="characterLength"
+    />
+    <span class="count">{{ characterLength }}</span>
+  </fieldset>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/sass/variables" as *;
+
+fieldset {
+  border: none;
+  margin: 0;
+  padding: 1rem;
+  position: relative;
+
+  label {
+    display: block;
+    @include font-body();
+  }
+
+  input[type="range"] {
+    width: 100%;
+    margin-top: 1rem;
+  }
+
+  span.count {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 1rem;
+    color: $color-neon-green;
+    @include font-heading-m();
+  }
+}
+</style>
