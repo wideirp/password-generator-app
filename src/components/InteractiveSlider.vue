b74fc3e49180455e05af014cb3e65c3120b777bb
wideirp <51214984+wideirp@users.noreply.github.com>
2024-08-28 18:48:21 -0500

    Change to v-bind from v-model on slider

--- src/components/InteractiveSlider.vue
+++ src/components/InteractiveSlider.vue
@@ -12,7 +12,7 @@ import { store } from "@/store";
       min="6"
       max="16"
       step="1"
-      v-model="store.characterLength"
+      :value="store.characterLength"
     />
     <span class="count">{{ store.characterLength }}</span>
   </fieldset>
