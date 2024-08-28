9374f8876bc3b16212229594f28be9b9ce48a8ba
wideirp <51214984+wideirp@users.noreply.github.com>
2024-08-28 18:28:46 -0500

    Add shuffle function

--- src/PasswordGenerator.js
+++ src/PasswordGenerator.js
@@ -3,8 +3,17 @@ export default class PasswordGenerator {
     this.lowerLetters = "abcdefghijklmnopqrstuvwxyz";
     this.upperLetters = this.lowerLetters.toUpperCase();
     this.numbers = "1234567890";
-    this.symbols = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    this.symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   }
+
+  shuffleString = function (str) {
+    const chars = str.split("");
+    for (let i = chars.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [chars[i], chars[j]] = [chars[j], chars[i]];
+    }
+    return chars.join("");
+  };
 
   generatePassword = function (charLen, lower, upper, nums, symbols) {
     let charList = "";
@@ -13,6 +22,7 @@ export default class PasswordGenerator {
     charList += nums ? this.numbers : "";
     charList += symbols ? this.symbols : "";
 
-    return charList.slice(0, charLen);
+    const shuffledString = this.shuffleString(charList);
+    return shuffledString.slice(0, charLen);
   };
 }
