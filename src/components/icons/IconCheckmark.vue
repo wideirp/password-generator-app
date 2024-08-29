4d1abfa63fe03b710346c2db18866ea47f7927b1
wideirp <51214984+wideirp@users.noreply.github.com>
2024-08-28 23:18:44 -0500

    Add icon

---
+++ src/components/icons/IconCheckmark.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg width="14" height="12" xmlns="http://www.w3.org/2000/svg">
+    <path
+      stroke="#18171F"
+      stroke-width="3"
+      fill="none"
+      d="M1 5.607 4.393 9l8-8"
+    />
+  </svg>
+</template>
+
+<style scoped lang="scss">
+@use "../../assets/sass/variables" as *;
+
+svg:hover {
+  & > path {
+    fill: $color-almost-white;
+  }
+}
+</style>
