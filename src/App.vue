48a06a52174af94dbad827660ccd7b954469f83e
wideirp <51214984+wideirp@users.noreply.github.com>
2024-08-28 22:25:36 -0500

    Add checkbox state

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <script setup>
-import { ref } from "vue";
 import PasswordDisplay from "./components/PasswordDisplay.vue";
 import InteractiveSlider from "./components/InteractiveSlider.vue";
 import OptionCheckbox from "./components/OptionCheckbox.vue";
 import IconArrow from "./components/icons/IconArrow.vue";
 import PasswordGenerator from "./PasswordGenerator";
+import { store } from "./store";
 
 const passGen = new PasswordGenerator();
-const pass = ref("");
 
 function onClick() {
-  const newPass = passGen.generatePassword(8, true, true, true, true);
-  pass.value = newPass;
+  const newPass = passGen.generatePassword(
+    store.characterLength,
+    store.includeLower,
+    store.includeUpper,
+    store.includeNumbers,
+    store.includeSymbols
+  );
+  store.password = newPass;
 }
 </script>
 
 <template>
   <main>
     <h1>Password Generator</h1>
-    <PasswordDisplay :password="pass" />
+    <PasswordDisplay :password="store.password" />
     <section>
       <InteractiveSlider />
-      <OptionCheckbox id="uppercase" text="Include Uppercase Letters" />
-      <OptionCheckbox id="lowercase" text="Include Lowercase Letters" />
-      <OptionCheckbox id="numbers" text="Include Numbers" />
-      <OptionCheckbox id="symbols" text="Include Symbols" />
+      <OptionCheckbox
+        id="uppercase"
+        text="Include Uppercase Letters"
+        v-model="store.includeUpper"
+      />
+      <OptionCheckbox
+        id="lowercase"
+        text="Include Lowercase Letters"
+        v-model="store.includeLower"
+      />
+      <OptionCheckbox
+        id="numbers"
+        text="Include Numbers"
+        v-model="store.includeNumbers"
+      />
+      <OptionCheckbox
+        id="symbols"
+        text="Include Symbols"
+        v-model="store.includeSymbols"
+      />
       <button class="btn" @click.prevent="onClick">
         Generate <IconArrow />
       </button>
--- src/components/OptionCheckbox.vue
+++ src/components/OptionCheckbox.vue
@@ -1,11 +1,26 @@
 <script setup>
+import { store } from "@/store";
+
+const checkedModel = defineModel({ type: Boolean });
+
+function update(e) {
+  e.preventDefault();
+  checkedModel.value = e.target.checked;
+  console.log(store);
+}
 defineProps({ id: String, text: String });
 </script>
 
 <template>
   <div>
     <label for="">{{ text }}</label>
-    <input type="checkbox" name="id" :id />
+    <input
+      type="checkbox"
+      name="id"
+      :id
+      @change="update"
+      :checked="checkedModel"
+    />
   </div>
 </template>
 
